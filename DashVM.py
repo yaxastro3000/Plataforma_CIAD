0d3e8432b264855e115eb1ca1f6de9e5c39431d6
yaxastro3000 <yaxkin.coronado@gmail.com>
2024-01-29 11:04:33 -0600

    Update DashVM.py

--- DashVM.py
+++ DashVM.py
@@ -1,6 +1,8 @@
 import dash
 from dash import dcc
 from dash import html
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
